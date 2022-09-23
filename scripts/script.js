831d791c7932866c8bcb56e29ee48395984ee629
Jean Augustin <jre.augustin@gmail.com>
2022-09-22 20:35:04 -0400

    Add next steps in JS file

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// on submit, collect email address, and if applicable, phone number
+// Run email through kicbox 
+// run phone number through Twilio lookup
+// if email not deliverable, collect IP and lookup results
+// if phone number not mobile, collect IP and lookup results
+// pass information to a db
+// report IP if more than 10 unique emails/phone numbers submitted from that IP in the last 60 minutes
