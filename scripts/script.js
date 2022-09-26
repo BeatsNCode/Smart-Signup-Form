29a3d8b1bef190e5258234c472680a2a567ddf39
Jean Augustin <jre.augustin@gmail.com>
2022-09-26 19:35:49 -0400

    Successfully access Form Data and log in console

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
+'use-strict';
+
+function logSentence() {
+    event.preventDefault()
+    console.log('recorded a click')
+
+    let email = document.getElementById('emailAddress').value;
+    console.log(email)
+
+    let phone = document.getElementById('phoneNumber').value;
+    if (phone) {
+        console.log(phone)
+    }
+
+}
+
 // on submit, collect email address, and if applicable, phone number
 // Run email through kicbox 
 // run phone number through Twilio lookup
@@ -5,3 +21,4 @@
 // if phone number not mobile, collect IP and lookup results
 // pass information to a db
 // report IP if more than 10 unique emails/phone numbers submitted from that IP in the last 60 minutes
+// Disable Submit button
